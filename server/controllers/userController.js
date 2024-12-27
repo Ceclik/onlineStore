d224a142169a49eb0a226bdc17237e1e36fbbae7
Ceclik <yura11122345@gmail.com>
2024-12-27 16:47:15 +0300

    Added user verification

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,10 +53,8 @@ class UserController{
     }
 
     async check(req, res, next) {
-        const {id} = req.query;
-        if(!id){
-            return next(apiError.badRequest('id is not defined'));
-        }
+        const token = generateJwt(req.user.id, req.user.email, req.user.role);
+       return res.json(token);
     }
 
     async delete(req, res){};
