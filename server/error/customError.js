643b588115fcdf5104653dcdb469cf7ebf0ebdf7
Ceclik <yura11122345@gmail.com>
2025-01-29 13:39:55 +0300

    code refactored

--- server/error/customError.js
+++
@@ -1,9 +0,0 @@
-class CustomError extends Error{
-    constructor(error) {
-        super();
-        this.statusCode = error.code;
-        this.message = error.message;
-    }
-}
-
-module.exports = CustomError;
